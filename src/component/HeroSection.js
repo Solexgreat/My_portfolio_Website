b5b9a38a8c7f01ba6b9325ffd30272f349ea981d
Solexgreat <solomonGodkid@gmail.com>
2024-07-25 11:35:56 +0100

    Nav and Hero

--- src/component/HeroSection.js
+++ src/component/HeroSection.js
@@ -7,10 +7,22 @@ export default function HeroSection() {
 				<img alt='' width={355.97} height={562}/>
 			</div>
 			<div className='textSection'>
-				<h1>Hi,</h1>
-				<h2>I'm Solomon,</h2>
-				<p>I'm a <span>Software <br/> Engineer</span></p>
+				<div className="text">
+					<h1>Hi,</h1>
+					<h2>I'm Solomon,</h2>
+					<p>I'm a <span>Software <br/> Engineer</span></p>
+				</div>
+				<div className='btn'>
+					<div className="contact-Me">
+						<button > Contact me</button>
+					</div>
+					<div className="view-project">
+						<button> View my project</button>
+					</div>
+					
 			</div>
+			</div>
+			
 		</div>
 	)
 }
--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 export default function Nav() {
+	const[menuOpen, setMenuOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setMenuOpen(!menuOpen);
+	}
 	return (
-		<nav className="nav-bar">
+		<nav className='nav-bar'>
 			<div className='Logo'>
 
 			</div>
-			<div className='nav-menu'>
-				<ul className='nav-item'>
-						<li className='nav-list'><a href='#Home' className='nav-link'>Home</a></li>
-						<li className='nav-list'><a href='#About' className='nav-link'>About</a></li>
-						<li className='nav-list'><a href='#Portfolio' className='nav-link'>Portfolio</a></li>
-					</ul>
-			</div>
+		  {/*mobile menu*/}
+			<div className="menu-bar" onClick={toggleMenu}>
+					<div className='bar'></div>
+					<div className='bar'></div>
+					<div className='bar'></div>
+				</div>
+
+			<ul className={`nav-item ${menuOpen ? "visible" : ""}`}>
+					<li ><a href='#Home' >Home</a></li>
+					<li ><a href='#About'>About</a></li>
+					<li ><a href='#Portfolio'>Portfolio</a></li>
+			</ul>
+
 		</nav>
 	)
 }
